d4d98d3c7fb8b912312f27f8672e9e58f694eb91
Jonas hofmann <Jonash871j@gmail.com>
2020-11-17 14:41:17 +0100

    Commit

--- GLEngine/main.cpp
+++ GLEngine/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include "engine.h"
 #include <GL/glew.h>
 
 using namespace Engine;
+
 
 int main()
 {
@@ -22,43 +23,52 @@ int main()
 	//renderer.SetShaderProgram(&shaderTest);
 	renderer.SetShaderProgram(&ShaderProgram::CreateSpriteProgram());
 
-	Texture texture("Images/gold_brick.png", Texture::Mode::Nearest);
+	enum Types
+	{
+		Stone,
+		Sccissors,
+		Paper,
+		Undefined,
+	};
 
-	Sprite sprite(&texture, 16, 8, 0);
+	Sprite sprType[3]
+	{ 
+		(new Texture("Images/Scissors.png")), 
+		(new Texture("Images/Rock.png")),
+		(new Texture("Images/Paper.png"))
+	};
 
-	Vector2D position;
-	Vector2D size = { 1.0f, 1.0f };
-	float rotation = 0.0f;
+	Sprite sprWin(new Texture("Images/win.png"));
+	Sprite sprLost(new Texture("Images/lost.png"));
+
+	int a = Undefined;
+	int b = Undefined;
 
 	while (!window.GetIsClosed())
 	{
 		if (input.KeyPressed(Key::Escape))
 			window.Close();
-
-		if (input.KeyPressed(Key::F1))
-			window.SetScaling(4, 4);
-		if (input.KeyPressed(Key::F2))
-			window.SetScaling(1, 1);
-
-
-		//position.x += (input.KeyState(Key::D) - input.KeyState(Key::A)) * time.GetDeltaTime() * 100.0f;
-		//position.y += (input.KeyState(Key::S) - input.KeyState(Key::W)) * time.GetDeltaTime() * 100.0f;
-		position = input.MousePosition();
-		size.x -= ((float)input.KeyState(Key::Left)) / 100.0f;
-		size.x += ((float)input.KeyState(Key::Right)) / 100.0f;
-		size.y -= ((float)input.KeyState(Key::Up)) / 100.0f;
-		size.y += ((float)input.KeyState(Key::Down)) / 100.0f;
-		rotation += ((float)input.KeyState(Key::R)) / 10.0f;
-		rotation -= ((float)input.KeyState(Key::F)) / 10.0f;
-
-		renderer.SpriteEx(&sprite, position, rotation, size);
-		renderer.Sprite(&sprite, { 0.0f, 0.0f }, { 1, 0});
-		renderer.Sprite(&sprite, { 0.0f, 64.0f });
+			
+		if (input.KeyPressed(Key::Space))
+		{
+			a = rand() % 2;
+			b = rand() % 2;
+		}	
 		
+		if (a != Undefined)
+		{
+			renderer.SpriteEx(&sprType[b], { 0, 0 }, 325.0f);
+			renderer.SpriteEx(&sprType[a], { 96, 0 }, 45.0f);
 
-
-		//Console::PrintMsg("X: %f Y: %f R: %f", position.x, position.y, rotation);
-
+			if ((a == Stone) && (b == Sccissors))
+				renderer.Sprite(&sprWin);
+			else if ((a == Sccissors) && (b == Paper))
+				renderer.Sprite(&sprWin);
+			else if ((a == Paper) && (b == Stone))
+				renderer.Sprite(&sprWin);
+			else if (a != b)
+				renderer.Sprite(&sprLost);
+		}
 
 		window.Update();
 	}
